4ceb0ecaf0f6451d0458ffc7346924be2407af92
Shital lokhande <lokhandeshital.com>
2023-01-31 18:04:14 +0530

    Changes By Shital

---
+++ src/test/java/com/BikkadIT/GitHub3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHub3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
